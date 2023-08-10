ebdd2f46a47824980f9357b19dd7cee92023f3cd
cristianrichard <cristianr9776@gmail.com>
2023-08-09 22:59:59 -0300

    navbar y estilos css nativo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
          <header>
             <div>
                <a href="#">
-                  <svg viewBox="100 100 50 32" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M 150.07 131.439 L 131.925 100 L 122.206 105.606 L 137.112 131.439 L 150.07 131.439 Z M 132.781 131.439 L 120.797 110.692 L 111.078 116.298 L 119.823 131.439 L 132.781 131.439 Z M 109.718 121.401 L 115.509 131.439 L 102.551 131.439 L 100 127.007 L 109.718 121.401 Z" fill="black"></path></svg>
+                  <svg viewBox="100 100 50 32" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M 150.07 131.439 L 131.925 100 L 122.206 105.606 L 137.112 131.439 L 150.07 131.439 Z M 132.781 131.439 L 120.797 110.692 L 111.078 116.298 L 119.823 131.439 L 132.781 131.439 Z M 109.718 121.401 L 115.509 131.439 L 102.551 131.439 L 100 127.007 L 109.718 121.401 Z" fill="black"></path></svg>
                </a>
             </div>
             <nav>
@@ -32,8 +32,8 @@ const NavBar = () => {
                   </li>
                </ul>
             </nav>
-            <div>
-               <div>
+            <div className='search-container'>
+               <div className='search'>
                   <input placeholder="Buscar" />
                   <div>
                      <span>
